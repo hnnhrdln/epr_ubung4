63e1f5a50a5b4b7cabef40b63a7b706ce35b89da
Breizhard <75618120+Breizhard@users.noreply.github.com>
2020-12-07 15:04:07 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
 __author__ = "5641727, Redelin, $Matrikelnummer, $Nachname"
+import random
+
+def roll_dice(number, faces, seed=None):
+    liste = []
+    for i in range (0,number):
+        liste.append(random.randrange(1,faces+1))
+    print(liste)
