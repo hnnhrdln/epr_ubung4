1f5d0e4f6b3652ed13610366f449cec8eb8a0409
hnnhrdln <hannah-redelin@web.de>
2020-12-07 15:45:24 +0100

    set seed, renamed some values, added super basic sixteen fut

--- script.py
+++ script.py
@@ -2,8 +2,26 @@ __author__ = "5641727, Redelin, $matrikelnummer, $name"
 
 import random
 
+random.seed(1234)
+
 def roll_dice(number, faces, seed=None):
-    liste = []                  #keine Ahnung ob das eine gute Idee ist die Liste so zu definieren
+    """
+    Returns list of thrown dice values. Implements at least one throw.
+    """
+    if number < 1:
+        number = 1
+
+    dice_throws = []                  #keine Ahnung ob das eine gute Idee ist die Liste so zu definieren
     for i in range (0,number):
-        liste.append(random.randrange(1,faces+1))
-    print(liste)
+        dice_throws.append(random.randrange(1,faces+1))
+    return dice_throws
+
+
+def sixteen_is_dead( players):
+    """
+    implementiert ablauf. 
+    players ist eine liste aus tupel
+    """
+    print(roll_dice(6,6, None))
+
+sixteen_is_dead("bla")
