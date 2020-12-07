0a8296324d8362030a98216426ebcee57ad095e9
hnnhrdln <hannah-redelin@web.de>
2020-12-07 16:05:42 +0100

    TODO ergänzt

--- script.py
+++ script.py
@@ -1,12 +1,29 @@
 __author__ = "5641727, Redelin, $matrikelnummer, $name"
 
 import random
-
 random.seed(1234)
+
+"""
+TODO
+Beginn:
+-players definieren: ('name des spielers', boolean: über eingabe oder auto)
+->vllt in eigener funktion user prompts zur konfigurierung schalten
+-user prompt über anzahl der würfel und augenzahl der würfel
+-nutzer zu beginn die spielregeln & eingaben erklären. [Return] soll roll_dice ausführen, [n] den "becher" weitergeben
+
+sixteen_is_dead:
+-logic umsetzen, aka 10er, 9er, gewinn/verlust koordinieren, spieler anzeigen
+->(random risikobereitschaft im pc implementieren? aka pc kann "entscheiden" sein glück zu versuchen und wirft nochmal?)
+
+allgemein:
+-error handling -> nicht abbruch bei falscheingabe
+-spiel beenden 
+-spiel neu starten
+"""
 
 def roll_dice(number, faces, seed=None):
     """
-    Returns list of thrown dice values. Implements at least one throw.
+    Returns list of thrown dice values. Implements at least one dice.
     """
     if number < 1:
         number = 1
@@ -23,5 +40,7 @@ def sixteen_is_dead( players):
     players ist eine liste aus tupel
     """
     print(roll_dice(6,6, None))
+
+
 
 sixteen_is_dead("bla")
