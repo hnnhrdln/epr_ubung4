aa080240b5bcf58e296b4b8babcb8655d8a270cc
hnnhrdln <hannah-redelin@web.de>
2020-12-08 13:20:09 +0100

    game set up

--- script.py
+++ script.py
@@ -20,6 +20,46 @@ allgemein:
 -spiel beenden 
 -spiel neu starten
 """
+#Game set-up
+
+print("Hi, lets play sixteen or dead.\nThe rules: Choose a number or dice and\
+throw them until you get as close to 16 as possible.\nBut beware! If you excede 16 you're out. Whoever is closest to 16 wins.\
+\nIf your throws add up to 9 you cannot throw again. If they\
+add up to 10, you have to throw again.\nEnjoy!")
+
+players = []
+
+while True:
+    try:
+        number_of_players = int(input("How many players do you want?"))
+        break
+    except: 
+        continue
+
+while isinstance(number_of_players, int) == False or number_of_players <= 0:
+    number_of_players = int(input("How many players do you want?"))
+
+for x in range(0, number_of_players):
+    name = input("What is the name of player "+str(x+1)+"?\n")
+    player_or_pc = input("Who is the player played by? [me/pc]\n")
+    while player_or_pc != "me" and player_or_pc != "pc":
+        player_or_pc = input("Try again! Who is the player played by? [me/pc]\n")
+    
+    if player_or_pc == "me":
+        player_or_pc = True
+    elif player_or_pc == "pc":
+        player_or_pc = False
+
+    player_tuple = (name, player_or_pc)
+
+    players.append(player_tuple)
+
+print(players)
+
+
+
+#some loop to fill lst
+
 
 def roll_dice(number, faces, seed=None):
     """
@@ -34,7 +74,7 @@ def roll_dice(number, faces, seed=None):
     return dice_throws
 
 
-def sixteen_is_dead( players):
+def sixteen_is_dead(players):
     """
     implementiert ablauf. 
     players ist eine liste aus tupel
@@ -42,5 +82,4 @@ def sixteen_is_dead( players):
     print(roll_dice(6,6, None))
 
 
-
-sixteen_is_dead("bla")
+sixteen_is_dead(players)
